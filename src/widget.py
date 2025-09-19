26894349a70a1f3b6b8b0b70de6d964f73fa7cef
Semenov Sergei <modnyava4@gmail.com>
2025-09-19 20:31:52 +1000

    add mask_card_account function

--- src/widget.py
+++ src/widget.py
@@ -1,1 +1,9 @@
 from masks import get_mask_account, get_mask_card_number
+
+
+def mask_card_account(input_data: str) -> str:
+    tmp_data = input_data.split(' ')
+    if 'счет' in input_data.lower():
+        return f'{" ".join(tmp_data[:-1])} {get_mask_account(tmp_data[-1])}'
+    else:
+        return f'{" ".join(tmp_data[:-1])} {get_mask_card_number(tmp_data[-1])}'
