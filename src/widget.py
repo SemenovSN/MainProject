dc67feaac901c289c85582b6b0693dea4e6cb0b9
Semenov Sergei <modnyava4@gmail.com>
2025-09-19 20:37:29 +1000

    add mask_card_account docstring

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,16 @@ from masks import get_mask_account, get_mask_card_number
 
 
 def mask_card_account(input_data: str) -> str:
+    """
+    Принимает один аргумент — строку, содержащую тип и номер карты или счета вида:
+    1. Visa Platinum XXXXXXXXXXXXXXXX
+    2. Счет XXXXXXXXXXXXXXXXXXXX
+    Возвращает строку с замаскированным номером вида:
+    1. Visa Platinum XXXX XX** **** XXXX
+    2. Счет **XXXX
+    :param input_data: строка с типом и номером карты/счета вида
+    :return: строка с замаскированным номером
+    """
     tmp_data = input_data.split(' ')
     if 'счет' in input_data.lower():
         return f'{" ".join(tmp_data[:-1])} {get_mask_account(tmp_data[-1])}'
