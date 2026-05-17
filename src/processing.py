a8a9a4c20ac25a1ffb9ad7afb02a6d880cccd04c
Sergey Semenov <modnyava4@gmail.com>
2026-05-17 18:54:06 +1000

    add filter_by_state function

--- src/processing.py
+++ src/processing.py
@@ -1,1 +1,7 @@
-12
+def filter_by_state(dictionary_list, state='EXECUTED'):
+    filter_by_state_result = []
+    for dictionary in dictionary_list:
+        for key, value in dictionary.items():
+            if key == 'state' and value == state:
+                filter_by_state_result.append(dictionary)
+    return filter_by_state_result
