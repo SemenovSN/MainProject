86b194dd95a4f7bba497442eea43c6a1f545e50f
Sergey Semenov <modnyava4@gmail.com>
2026-05-21 20:10:08 +1000

    add tests for get_date from src/widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,4 +1,5 @@
-from src.widget import mask_account_card
+import pytest
+from src.widget import mask_account_card, get_date
 
 
 def test_mask_account_card():
@@ -14,9 +15,31 @@ def test_mask_account_card():
     assert mask_account_card('Счет 1234567890123456') == 'Счет **3456'
     # тест - номер без указания типа (карта или счет)
     assert mask_account_card('1234567890123456') == ' 1234 56** **** 3456'
+    # тест - тип (счет) без номера счета
+    assert mask_account_card('Счет') == ' **С**ч**е**т**'
+    # тест - тип (карта) без номера карты
+    assert mask_account_card('1234567890123456') == ' 1234 56** **** 3456'
     # тест - некорректный формат ввода
+    assert mask_account_card('random string') == 'random stri ng'
     # тест - пустой аргумент
-    # тест - некорректный тип аргумента
+    with pytest.raises(TypeError):
+        mask_account_card()
 
 def test_get_date():
-    pass
+    # тест - корректная дата и время в формате ISO 8601
+    assert get_date('2000-01-01T00:00:00.000000') == '01.01.2000'
+    # тест - некорректная дата (в формате ДД.ММ.ГГГГ) и время в формате ISO 8601
+    assert get_date('01.01.2000T00:00:00.000000') == '01.01.2000'
+    # тест - корректная дата в формате ISO 8601 с сепаратором
+    assert get_date('2000-01-01T') == '01.01.2000'
+    # тест - корректное время в формате ISO 8601 с сепаратором
+    assert get_date('T00:00:00.000000') == ''
+    # тест - пустой аргумент
+    with pytest.raises(TypeError):
+        assert  get_date()
+    # тесты исключений
+    with pytest.raises(ValueError):
+        # тест - дата и время в формате ISO 8601 c с некорректным сепаратором
+        assert get_date('2000-01-01A00:00:00.000000') == 'Счет **7890'
+        # тест - дата в формате ISO 8601 без сепаратора
+        assert get_date('2000-01-01') == 'Счет **7890'
