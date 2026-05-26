500a92c4c64fd7587da9b40766a2cefcfa2cc53e
Sergey Semenov <modnyava4@gmail.com>
2026-05-26 21:06:25 +1000

    add card_number_generator func

--- src/generators.py
+++ src/generators.py
@@ -1,9 +1,19 @@
-def filter_by_currency(transactions: list, currency_code: str) -> dict:
-    for transaction in transactions:
+from typing import Iterator
+
+
+def filter_by_currency(transactions_list: list, currency_code: str) -> Iterator[dict]:
+    for transaction in transactions_list:
         condition_check = transaction['operationAmount']['currency']['code']
         if condition_check == currency_code:
             yield transaction
 
-def transaction_descriptions(transactions: list) -> str:
-    for transaction in transactions:
+def transaction_descriptions(transactions_list: list) -> Iterator[str]:
+    for transaction in transactions_list:
         yield transaction["description"]
+
+def card_number_generator(start: int, stop: int) -> Iterator[str]:
+    for generated_number in range(start, stop):
+        generated_number = str(generated_number)
+        template = '0' * (16 - len(generated_number)) + str(generated_number)
+        card_number = f'{template[:4]} {template[4:8]} {template[8:12]} {template[12:]}'
+        yield card_number
