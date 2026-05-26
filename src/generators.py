3632fc0cacc37dbcdb98819921562a865b249001
Sergey Semenov <modnyava4@gmail.com>
2026-05-26 20:38:57 +1000

    add transaction_description func

--- src/generators.py
+++ src/generators.py
@@ -3,3 +3,7 @@ def filter_by_currency(transactions: list, currency_code: str) -> dict:
         condition_check = transaction['operationAmount']['currency']['code']
         if condition_check == currency_code:
             yield transaction
+
+def transaction_descriptions(transactions: list) -> str:
+    for transaction in transactions:
+        yield transaction["description"]
